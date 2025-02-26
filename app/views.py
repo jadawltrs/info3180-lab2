18a609a605758caee32cc59a27e7032e6dc99db3
jadawltrs <jada.walters@mymona.uwi.edu>
2025-02-25 23:26:30 -0500

    completed lab

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+import datetime
 
 ###
 # Routing for your application.
@@ -15,7 +15,16 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Jada Walters")
+
+def format_date_joined(date):
+    return date.strftime("%b, %Y")
+
+@app.route('/profile')
+def profile():
+    date_joined = datetime.datetime(2018, 1, 1)
+    formatted_date = format_date_joined(date_joined)
+    return render_template('profile.html', date_joined = formatted_date, name = "Jada Walters")
 
 
 ###
@@ -45,3 +54,5 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+
